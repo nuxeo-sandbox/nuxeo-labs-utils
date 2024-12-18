fefc4ae3d5246d3337b05601dd81b5ce5972b4db
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-12-18 17:01:41 +0900

    Fix import

--- nuxeo-labs-utils-core/src/main/java/nuxeo/labs/utils/DocTypeIconThumbnailFactory.java
+++ nuxeo-labs-utils-core/src/main/java/nuxeo/labs/utils/DocTypeIconThumbnailFactory.java
@@ -24,7 +24,7 @@ import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import jakarta.imageio.ImageIO;
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
